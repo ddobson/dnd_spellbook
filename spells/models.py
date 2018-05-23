2b752cee86d052ff744c1be430313174036bf8d8
Dale Dobson <ddobson445@gmail.com>
2018-05-23 12:21:26 -0700

    Create Spellbook Model

--- spells/models.py
+++ spells/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField, JSONField
 
 
+DEFAULT_USER_ID = 1
+
+
+class Spellbook(models.Model):
+    name = models.CharField(blank=False, null=False, default="Generic Spellbook", max_length=64)
+    description = models.CharField(blank=True, null=True, max_length=256)
+    classes = ArrayField(
+        models.CharField(max_length=24, blank=True),
+        size=24
+    )
+
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, default=DEFAULT_USER_ID)
+
+
 class Spell(models.Model):
     casting_time = models.CharField(blank=True, null=True, max_length=128)
     classes = ArrayField(
