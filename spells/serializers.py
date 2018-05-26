37ac8ba5f9e558c3d6385349aa0f15588753eceb
Dale Dobson <ddobson445@gmail.com>
2018-05-25 20:38:44 -0700

    Add spellbook spell update logic

--- spells/serializers.py
+++ spells/serializers.py
@@ -27,6 +27,16 @@ class SpellSerializer(ModelSerializer):
 class SpellbookSerializer(ModelSerializer):
     user = HiddenField(default=CurrentUserDefault())
 
+    class Meta:
+        model = Spellbook
+        fields = (
+            'id',
+            'name',
+            'description',
+            'classes',
+            'user',
+        )
+
     def __init__(self, *args, **kwargs):
         super(SpellbookSerializer, self).__init__(*args, **kwargs)
 
@@ -37,21 +47,30 @@ class SpellbookSerializer(ModelSerializer):
             self.fields['spells'] = SpellSerializer(
                 many=True, context=kwargs['context'])
 
+    def find_spells(self, spell_ids):
+        spells = set()
+        for spell in spell_ids:
+            spells.add(Spell.objects.get(pk=spell['id']))
+        return spells
+
+    def process_spellbook_spell_updates(self, spellbook, request_spells_set, instance_spells_set):
+        spellbook.spells.add(*request_spells_set.difference(instance_spells_set))
+        spellbook.spells.remove(*instance_spells_set.difference(request_spells_set))
+
     @transaction.atomic
     def create(self, validated_data):
         spellbook = Spellbook.objects.create(**validated_data)
         if 'spells' in self.initial_data:
-            spell_ids = self.initial_data.get('spells')
-            spells = Spell.objects.filter(pk__in=[spell['id'] for spell in spell_ids])
+            spells = self.find_spells(self.initial_data.get('spells'))
             spellbook.spells.add(*spells)
-        return spellbook
+            return spellbook
 
-    class Meta:
-        model = Spellbook
-        fields = (
-            'id',
-            'name',
-            'description',
-            'classes',
-            'user',
-        )
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        if 'spells' in self.initial_data:
+            request_spells_set = self.find_spells(self.initial_data.get('spells'))
+            instance_spells_set = set(instance.spells.all())
+            self.process_spellbook_spell_updates(instance, request_spells_set, instance_spells_set)
+        instance.__dict__.update(**validated_data)
+        instance.save()
+        return instance
