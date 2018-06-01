ef713e093e97ad8076f943153a9f2e36b2ecf9ed
Dale Dobson <ddobson445@gmail.com>
2018-05-31 21:39:19 -0700

    Move default user into constants file

--- dnd_spellbook/utils/constants.py
+++ dnd_spellbook/utils/constants.py
@@ -9,6 +9,7 @@ VALID_CHARACTER_CLASSES = [
     'wizard',
 ]
 
+DEFAULT_USER_ID = 1
 SPELL_DOES_NOT_CONTAIN_ID = "One or more of the spells you tried to add/udpate did not contain an `id` key."
 CLASSES_VALIDATION_ERROR = "Cannot add an invalid class to a spellbook. VALID CLASSES: {}".format(VALID_CHARACTER_CLASSES)  # noqa
 SPELLBOOK_SPELL_NOT_FOUND_ERROR = "One or more of the spells you tried to add could not be found."
--- spells/models/spellbook.py
+++ spells/models/spellbook.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField, JSONField
+from dnd_spellbook.utils import constants as const
 from spells import validators
 from spells.models.spell import Spell
 from spells.validators import validate_spell_classes
-
-DEFAULT_USER_ID = 1
 
 
 class Spellbook(models.Model):
@@ -14,7 +13,7 @@ class Spellbook(models.Model):
     name = models.CharField(blank=False, null=False, default="Generic Spellbook", max_length=64)
     description = models.CharField(blank=True, null=True, max_length=256)
     classes = ArrayField(models.CharField(max_length=24, blank=True), default=list, size=24)
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE, default=DEFAULT_USER_ID)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, default=const.DEFAULT_USER_ID)
     spells = models.ManyToManyField(
         Spell,
         related_name="spellbooks",
